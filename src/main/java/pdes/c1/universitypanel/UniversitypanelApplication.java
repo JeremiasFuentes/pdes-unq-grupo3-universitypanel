c61df632663e8617472fcbb78a98f6092751dcc4
matinicolosi <mati.nicolosi@gmail.com>
2023-05-17 23:53:37 -0300

    develop: Configuracion de swagger

--- src/main/java/pdes/c1/universitypanel/UniversitypanelApplication.java
+++ src/main/java/pdes/c1/universitypanel/UniversitypanelApplication.java
@@ -2,10 +2,8 @@ package pdes.c1.universitypanel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan("pdes.c1.universitypanel")
 public class UniversitypanelApplication {
 
 	public static void main(String[] args) {
